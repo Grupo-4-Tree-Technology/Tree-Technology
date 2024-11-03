3894735c26b8c5ce4817db8dbd279d7e9b2ff178
Gustavo-Kohatsu <gustavo.kohatsu@sptech.school>
2024-11-03 12:47:35 -0300

    criacao da sessao entre em contato + funcionalidade de enviar e-mail para a conta corporativa tree technology + criacao de novas tabelas do modelo fisico

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,19 +6,77 @@ CREATE TABLE IF NOT EXISTS empresa (
 id 			 	INT AUTO_INCREMENT,
 razao_social 	VARCHAR(100) NOT NULL UNIQUE,
 nome_fantasia 	VARCHAR(45) NOT NULL,
-email 			VARCHAR(345) NOT NULL UNIQUE,
 cnpj 			VARCHAR(18) NOT NULL UNIQUE,
-senha 			VARCHAR(16) NOT NULL,
 
-PRIMARY KEY pk_empresa (id)
+PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS usuario (
+id 					INT AUTO_INCREMENT,
+nome 				VARCHAR(45) NOT NULL,
+cpf 				CHAR(11) NOT NULL UNIQUE,
+email 				VARCHAR(45) NOT NULL UNIQUE,
+senha 				VARCHAR(16) NOT NULL,
+permissao 			CHAR(7) NOT NULL,
+status 				CHAR(10) NOT NULL,
+data_contratacao 	DATE NOT NULL,
+fkEmpresa 			INT NOT NULL,
+
+PRIMARY KEY (id),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (id),
+CONSTRAINT CHECK (permissao IN ('total', 'leitura')),
+CONSTRAINT CHECK (status IN ('ativado', 'desativado'))
+);
+
+CREATE TABLE IF NOT EXISTS recomendacao (
+id 			INT AUTO_INCREMENT,
+descricao 	TEXT NOT NULL,
+data_hora 	DATETIME NOT NULL,
+fkEmpresa 	INT NOT NULL,
+
+PRIMARY KEY (id),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (id)
+);
+
+CREATE TABLE IF NOT EXISTS prompt_entrada (
+id 			INT AUTO_INCREMENT,
+pergunta 	TEXT NOT NULL,
+data_hora 	DATETIME NOT NULL,
+fkEmpresa 	INT NOT NULL,
+
+PRIMARY KEY (id),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (id)
+);
+
+CREATE TABLE IF NOT EXISTS prompt_saida (
+id 				INT AUTO_INCREMENT,
+resposta 		TEXT NOT NULL,
+data_hora 		DATETIME NOT NULL,
+fkPromptEntrada INT NOT NULL,
+
+PRIMARY KEY (id),
+FOREIGN KEY (fkPromptEntrada) REFERENCES prompt_entrada (id)
+);
+
+CREATE TABLE IF NOT EXISTS notificacao (
+id 			INT AUTO_INCREMENT,
+titulo 		TEXT NOT NULL,
+descricao 	TEXT NOT NULL,
+data_hora 	DATETIME NOT NULL,
+fkEmpresa 	INT NOT NULL,
+
+PRIMARY KEY (id),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (id)
 );
 
 CREATE TABLE IF NOT EXISTS veiculo (
 placa 		VARCHAR(7),
+modelo		VARCHAR(45) NOT NULL,
+ano			INT NOT NULL,
 fkEmpresa 	INT NOT NULL,
 
-PRIMARY KEY pk_placa (placa),
-FOREIGN KEY ForeignKey_fkEmpresa (fkEmpresa) REFERENCES empresa (id)
+PRIMARY KEY (placa),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (id)
 );
 
 CREATE TABLE IF NOT EXISTS rota (
@@ -26,16 +84,17 @@ id 				INT NOT NULL AUTO_INCREMENT,
 ponto_partida 	VARCHAR(100) NOT NULL,
 ponto_destino 	VARCHAR(100) NOT NULL,
 
-PRIMARY KEY pk_rota (id)
+PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS rotas_has_veiculo (
-fkRota 	INT NOT NULL,
-fkPlaca VARCHAR(7) NOT NULL,
+CREATE TABLE IF NOT EXISTS veiculo_rota (
+fkRota 			INT NOT NULL,
+fkPlaca 		VARCHAR(7) NOT NULL,
+tempo_estimado 	TIME NOT NULL,
 
-PRIMARY KEY pk_rotas_has_veiculo (fkRota, fkPlaca),
-FOREIGN KEY ForeignKey_fkRota (fkRota) REFERENCES rota (id),
-FOREIGN KEY ForeignKey_fkPlaca (fkPlaca) REFERENCES veiculo (placa)
+PRIMARY KEY (fkRota, fkPlaca),
+FOREIGN KEY (fkRota) REFERENCES rota (id),
+FOREIGN KEY (fkPlaca) REFERENCES veiculo (placa)
 );
 
 CREATE TABLE IF NOT EXISTS ruas_intermediarias (
@@ -44,8 +103,8 @@ rua 	VARCHAR(100) NOT NULL,
 ordem 	INT NOT NULL,
 fkRota 	INT NOT NULL,
 
-PRIMARY KEY pk_ruas_intermediarias (id),
-FOREIGN KEY ForeignKey_fkRota_ruas (fkRota) REFERENCES rota (id)
+PRIMARY KEY (id),
+FOREIGN KEY (fkRota) REFERENCES rota (id)
 );
 
 CREATE TABLE IF NOT EXISTS acidente_transito (
@@ -60,7 +119,7 @@ fase_dia 				VARCHAR(45) NOT NULL,
 condicao_metereologica 	VARCHAR(45) NOT NULL,
 qtd_veiculos_envolvidos INT NOT NULL,
 
-PRIMARY KEY pk_evento_transito (id),
+PRIMARY KEY (id),
 CONSTRAINT CHECK (fase_dia IN ('Plena Noite', 'Amanhecer', 'Pleno dia', 'Anoitecer')),
 CONSTRAINT CHECK (condicao_metereologica IN ('Ceu Claro', 'Chuva', 'Sol', 'Nublado', 'Garoa/Chuvisco'))
 );
@@ -73,5 +132,17 @@ arquivo_lido 			VARCHAR(45),
 titulo 					TEXT NOT NULL,
 descricao 				TEXT NOT NULL,
 
-PRIMARY KEY pk_log (id)
+PRIMARY KEY (id)
 );
+
+INSERT INTO empresa
+(razao_social, nome_fantasia, cnpj)
+VALUES
+('Tree Technology Brasil Ltda', 'Tree Technology', '39.529.500/0001-50'),
+('CET - Centro de Ensino Técnico', 'CET - Centro de Ensino Técnico', '42.411.685/0001-09');
+
+INSERT INTO usuario
+(nome, cpf, email, senha, permissao, status, data_contratacao, fkEmpresa)
+VALUES
+('Robson', '49123956846', 'robson@gmail.com', '123', 'total', 'ativado', '2022-11-03', 1),
+('Jair', '91723956168', 'jair.j@gmail.com', '321', 'total', 'ativado', '2015-01-11', 2);
